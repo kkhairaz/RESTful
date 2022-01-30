d6b1bd4c71f1a3c19f89b4a52ac288da78d72cdf
kkhairaz <kkhairaz@outlook.com>
2022-01-30 18:53:31 +0000

    Updated Controller

---
+++ src/main/java/com/example/mySpringProject/controller/pilotController.java
@@ -0,0 +1,46 @@
+package com.example.mySpringProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import java.util.List;
+import java.lang.Iterable;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/pilots")
+public class pilotController {
+    private final pilotRepository pilotRepository;
+
+    public SuperHeroController(pilotRepository pilotRepository) {
+      this.pilotRepository = pilotRepository;
+
+    }
+
+    @GetMapping()
+    public Iterable<pilot> getPilots() {
+        Iterable<pilot> pilots = pilotRepository.findAll();
+        return pilots;
+    }
+
+    @PostMapping(path = "/addNew")
+    public String createNewPilot(@RequestBody pilot p) {
+        pilotRepository.save(p);
+        return "New Pilot successfully added!";
+    }
+
+}
+
+    @GetMapping(path = "/{id}")
+    public Iterable<pilot> getPilotById(@PathVariable String id) {
+        Iterable<pilot> the_pilot = pilotRepository.findById(id);
+        return the_pilot;
+    }
+}
